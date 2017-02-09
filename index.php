a2e4177de814a0cd2b0e341338721121af15f328
dhoni <dhoni.p.saputra@gmail.com>
2017-02-09 19:17:44 +0700

    alayers 1.0.0

--- index.php
+++ index.php
@@ -1,6 +1,10 @@
 <!DOCTYPE html>
 <html>
 <head>
+	<meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    
 	<title>Alayer Converter</title>
 	<script type="text/javascript" src="http://code.jquery.com/jquery.min.js"></script>
 	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
@@ -31,7 +35,7 @@
 				<div class="convert" style=""> <h1 class="text-center text-muted"> Anda belum jadi alay<br><small class=""> Silahkan isikan textarea diatas untuk dikonversi menjadi alay version </small> </h1> </div>
 				<hr>
 				<button class="btn btn-warning btn-sm btn-copy" data-toggle="tooltip" title="Tulisan alay sudah disalin!" data-trigger="click" onclick="alaycopy()"> Salin </button> 
-				<button class="btn btn-danger btn-sm" onclick="$('.convert').html('')"> Bersihkan </button> 
+				 <button class="btn btn-danger btn-sm" onclick="$('.convert').html('')"> Bersihkan </button> 
 			</fieldset>
 
 			<textarea class="sr-only" placeholder="text yang akan dijadikan alay" name="copy"></textarea>
